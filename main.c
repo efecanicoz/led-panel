44c77785dd44616de75be7569e26e9b48a3b8da4
Efe Can İçöz <efecanicoz@gmail.com>
2016-02-08 13:23:30 +0200

    Pin states updated, new ext array and other pins are in unconnected state.

--- main.c
+++ main.c
@@ -2,14 +2,13 @@
 #include "ch.h"
 
 //a0,a1,a4,a5,a6,a7,a9,a10
-#define A0TO10 0b11011110011U
-#define MASKA0A1 0b11U
+#define A4TO10 0b11011110000U
 #define MASKA4A7 0b11110000U
 #define MASKA9A10 0b11000000000U
 
 /*Macros*/
-#define SOH 0x01
-#define EOT 0x04
+#define SOH 0x9A
+#define EOT 0xD5
 #define ID  0xF0
 
 /*Prototypes*/
@@ -24,23 +23,15 @@ static void extcb2(EXTDriver *extp, expchannel_t channel)
 	
 	chSysLockFromISR();
 	/* we must reinit clocks after waking up ESPECIALLY if use HSE or HSI+PLL */
-	//stm32_clock_init();
+	stm32_clock_init();
 
-	/*extChannelDisableI(&EXTD1, 0);
-	extChannelDisableI(&EXTD1, 1);
-	extChannelDisableI(&EXTD1, 4);
-	extChannelDisableI(&EXTD1, 5);
-	extChannelDisableI(&EXTD1, 6);
-	extChannelDisableI(&EXTD1, 7);
-	extChannelDisableI(&EXTD1, 9);
-	extChannelDisableI(&EXTD1, 10);*/
 	chSysUnlockFromISR();
 }
 
 static const EXTConfig extcfg = {
   {
-   {EXT_CH_MODE_RISING_EDGE | EXT_MODE_GPIOA, extcb2},
-   {EXT_CH_MODE_RISING_EDGE | EXT_MODE_GPIOA, extcb2},
+   {EXT_CH_MODE_DISABLED, NULL},
+   {EXT_CH_MODE_DISABLED, NULL},
    {EXT_CH_MODE_DISABLED, NULL},
    {EXT_CH_MODE_DISABLED, NULL},
    {EXT_CH_MODE_RISING_EDGE | EXT_MODE_GPIOA, extcb2},
@@ -68,11 +59,7 @@ static void prepareForSend(uint8_t *packet, uint8_t msg)
 {
 	uint8_t encoded, temp;
 	//logarithm(Math.h) or 8 if, another solution for encode ?
-	if(msg == 0x01)
-		encoded = 0;
-	else if(msg == 0x02)
-		encoded = 1;
-	else if(msg == 0x04)
+	if(msg == 0x04)
 		encoded = 2;
 	else if(msg == 0x08)
 		encoded = 3;
@@ -85,9 +72,9 @@ static void prepareForSend(uint8_t *packet, uint8_t msg)
 	else if(msg == 0x80)
 		encoded = 7;
 	
-	packet[0] = SOH;
-	//add id
+	//device id
 	packet[1] = ID;
+	
 	//add message
 	packet[1] |= encoded<<1;
 	//add parity
@@ -96,7 +83,6 @@ static void prepareForSend(uint8_t *packet, uint8_t msg)
 	temp = temp ^ temp >>4;
 	packet[1] = packet[1] | (temp & 1);
 	
-	packet[2] = EOT;
 	return;
 }
 
@@ -105,11 +91,8 @@ static void sleep(void)
 	/*Stop mode*/
 	PWR->CR |= (PWR_CR_LPDS | PWR_CR_CSBF | PWR_CR_CWUF);
 	PWR->CR &= ~PWR_CR_PDDS;
-	SCB->SCR |= SCB_SCR_SLEEPDEEP_Msk;//*/
-	/*Standby mode*/
-	/*PWR->CR |= (PWR_CR_PDDS | PWR_CR_CSBF | PWR_CR_CWUF);
-	SCB->SCR |= SCB_SCR_SLEEPDEEP_Msk;*/
-	
+	SCB->SCR |= SCB_SCR_SLEEPDEEP_Msk;
+	//wait for interrupt (enter sleep)
 	__WFI();
 }
 
@@ -123,8 +106,6 @@ int main(void)
    * Activates the EXT driver 1.
    */
 	extStart(&EXTD1, &extcfg);
-	extChannelEnable(&EXTD1, 0);
-	extChannelEnable(&EXTD1, 1);
 	extChannelEnable(&EXTD1, 4);
 	extChannelEnable(&EXTD1, 5);
 	extChannelEnable(&EXTD1, 6);
@@ -134,34 +115,41 @@ int main(void)
 	uint16_t rawSample = 0;
 	uint8_t portSample;
 	uint8_t packet[3];
+	
 	palClearPad(GPIOF, 0);
 	palSetPadMode(GPIOF, 0, PAL_MODE_OUTPUT_PUSHPULL);
-    palSetGroupMode(GPIOA, A0TO10, 0, PAL_MODE_INPUT_PULLDOWN);
-	palSetPadMode(GPIOA, GPIOA_USART_TX, PAL_MODE_ALTERNATE(1)); // used function : USART1_TX
-	palSetPadMode(GPIOA, GPIOA_USART_RX, PAL_MODE_ALTERNATE(1)); // used function : USART1_RX
+    palSetGroupMode(GPIOA, A4TO10, 0, PAL_MODE_INPUT_PULLDOWN);
+	palSetPadMode(GPIOA, GPIOA_USART_TX, PAL_MODE_ALTERNATE(1)); // used function :  USART1_TX
 	
+	palSetPadMode(GPIOA, GPIOA_USART_RX, PAL_MODE_UNCONNECTED); // used function : NOT USART1_RX
+	palSetPadMode(GPIOF, GPIOF_OSC_OUT, PAL_MODE_UNCONNECTED);
+	palSetPadMode(GPIOB, GPIOB_PIN1, PAL_MODE_UNCONNECTED);
+	palSetPadMode(GPIOA, GPIOA_PIN0, PAL_MODE_UNCONNECTED);
+	palSetPadMode(GPIOA, GPIOA_LED_GREEN, PAL_MODE_UNCONNECTED);
+	
+	//prepare constant packet bytes
+	packet[0] = SOH;
+	packet[2] = EOT;
 
 	uint8_t i;
 	while(!0)
 	{
+		sleep();
 		portSample = 0;
-		rawSample = palReadGroup(GPIOA, A0TO10, 0);
-		portSample |= (MASKA0A1 & rawSample);
+		rawSample = palReadGroup(GPIOA, A4TO10, 0);
 		portSample |= (MASKA4A7 & rawSample) >> 2;
 		portSample |= (MASKA9A10 & rawSample) >> 3;
-		if(portSample != 0x00)//if its different send (i think it's unnecessary with sleep)
+		
+		prepareForSend(packet, portSample);
+		palSetPad(GPIOF, 0);
+		sdStart(&SD1, NULL);
+		for(i = 0; i < 20U; i++)/*Send button five times, why five ?*/
 		{
-			prepareForSend(packet, portSample);
-			palSetPad(GPIOF, 0);
-			sdStart(&SD1, NULL);
-			for(i = 0; i < 20U; i++)/*Send button five times, why five ?*/
-			{
-				sdWrite(&SD1, (uint8_t *)packet, 3);
-			}
-			sdStop(&SD1);
-			palClearPad(GPIOF, 0);
+			sdWrite(&SD1, (uint8_t *)packet, 3);
 		}
-		sleep();
+		sdStop(&SD1);
+		palClearPad(GPIOF, 0);
+		
 	}
 	
 	return 0;
